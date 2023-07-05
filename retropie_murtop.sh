f3f76bce52b7afcf6812982788fdbb4fe5fbcf29
hiulit <hiulit@gmail.com>
2023-07-05 18:08:11 +0200

    Initial commit

---
+++ retropie_murtop.sh
@@ -0,0 +1,188 @@
+#!/usr/bin/env bash
+# retropie-murtop.sh
+#
+# RetroPie Murtop
+# A script to install Murtop on RetroPie.
+#
+# Author: hiulit
+# Repository: https://github.com/hiulit/RetroPie-Murtop
+# License: MIT https://github.com/hiulit/RetroPie-Murtop/blob/master/LICENSE)
+
+# Globals ####################################################################
+
+user="$SUDO_USER"
+[[ -z "$user" ]] && user="$(id -un)"
+
+home="$(eval echo ~$user)"
+
+readonly RP_DIR="$home/RetroPie"
+readonly RP_PORTS_DIR="/opt/retropie/ports"
+
+readonly SCRIPT_VERSION="1.0.0"
+readonly SCRIPT_DIR="$(cd "$(dirname $0)" && pwd)"
+readonly SCRIPT_NAME="$(basename "$0")"
+readonly SCRIPT_FULL="$SCRIPT_DIR/$SCRIPT_NAME"
+readonly SCRIPT_TITLE="RetroPie Murtop"
+readonly SCRIPT_DESCRIPTION="A script to install Murtop on RetroPie."
+
+readonly SCRIPTMODULE_NAME="murtop"
+readonly SCRIPTMODULE_PATH_FILE="scriptmodules/ports/$SCRIPTMODULE_NAME.sh"
+
+readonly GAME_BIN_DIR="$SCRIPT_DIR/bin"
+
+
+# Variables ##################################################################
+
+RP_SETUP_DIR="$home/RetroPie-Setup"
+
+
+# Functions ##################################################################
+
+function is_sudo() {
+    [[ "$(id -u)" -eq 0 ]]
+}
+
+
+function is_retropie() {
+    [[ -d "$RP_DIR" && -d "$home/.emulationstation" && -d "/opt/retropie" ]]
+}
+
+
+function check_retropie_setup_dir_path() {
+    if [[ -n "$1" ]]; then
+        if [[ -d "$1" ]]; then
+            RP_SETUP_DIR="$1"
+        else
+            echo "ERROR: '$1' doesn't exist!" >&2
+            exit 1
+        fi
+    fi
+}
+
+
+function usage() {
+    echo "USAGE: sudo $0 [OPTIONS]"
+    echo
+    echo "Use 'sudo $0 --help' to see all the options."
+}
+
+
+function get_game_version() {
+    local game_zip_file
+    local game_version
+
+    game_zip_file="$(find "$GAME_BIN_DIR" -type f -name "*.zip")"
+    game_version="$(echo "$(basename "$game_zip_file")" | cut -d "_" -f 2)"
+
+    echo "$game_version"
+}
+
+
+function get_options() {
+    if [[ -z "$1" ]]; then
+        usage
+        exit 0
+    else
+        case "$1" in
+#H -h, --help               Prints the help message.
+            -h|--help)
+                echo "$SCRIPT_TITLE"
+                for ((i=1; i<="${#SCRIPT_TITLE}"; i+=1)); do [[ -n "$dashes" ]] && dashes+="-" || dashes="-"; done && echo "$dashes"
+                echo "$SCRIPT_DESCRIPTION"
+                echo
+                echo "USAGE: sudo $0 [OPTIONS]"
+                echo
+                echo "OPTIONS:"
+                echo
+                sed '/^#H /!d; s/^#H //' "$0"
+                exit 0
+                ;;
+#H -i, --install [path]     Installs Murtop on RetroPie.
+#H                              Path: The location of the "RetroPie-Setup" folder.
+#H                              Default: "~/RetroPie-Setup".
+            -i|--install)
+                check_retropie_setup_dir_path "$2"
+                cat "$SCRIPT_DIR/$SCRIPTMODULE_PATH_FILE" > "$RP_SETUP_DIR/$SCRIPTMODULE_PATH_FILE"
+                "$RP_SETUP_DIR/retropie_packages.sh" "$SCRIPTMODULE_NAME"
+
+                if [[ "$?" -eq 0 ]]; then
+                    echo
+                    echo "Murtop was successfully installed!"
+                    echo
+                    echo "############################################################"
+                    echo " __  __            _              "
+                    echo "|  \/  |_   _ _ __| |_ ___  _ __  "
+                    echo "| |\/| | | | | '__| __/ _ \| '_ \ "
+                    echo "| |  | | |_| | |  | || (_) | |_) |"
+                    echo "|_|  |_|\__,_|_|   \__\___/| .__/ "
+                    echo "                           |_|    "
+                    echo "v"$(get_game_version)""
+                    echo
+                    echo "A fast-paced 80's arcade game where Dig Dug meets Bomberman."
+                    echo
+                    echo "Help Murti save the world from an invasion of"
+                    echo "carrot-hungry moles with her bomb-pooping skills!"
+                    echo
+                    echo "A game by hiulit © 2023"
+                    echo
+                    echo "############################################################"
+                else
+                    echo
+                    echo "ERROR: Couldn't install Murtop." >&2
+                fi
+                ;;
+#H -u, --uninstall [path]   Uninstalls Murtop on RetroPie.
+#H                              Path: The location of the "RetroPie-Setup" folder.
+#H                              Default: "~/RetroPie-Setup".
+            -u|--uninstall)
+                check_retropie_setup_dir_path "$2"
+
+                if [[ ! -f "$RP_SETUP_DIR/$SCRIPTMODULE_PATH_FILE" ]]; then
+                    echo "Can't uninstall Murtop because it is not installed."
+                    exit 1
+                fi
+
+                if [[ -d "$RP_PORTS_DIR/$SCRIPTMODULE_NAME" ]]; then
+                    "$RP_SETUP_DIR/retropie_packages.sh" "$SCRIPTMODULE_NAME" remove
+                fi
+
+                rm "$RP_SETUP_DIR/$SCRIPTMODULE_PATH_FILE"
+
+               if [[ "$?" -eq 0 ]]; then
+                    echo
+                    echo "Murtop was successfully uninstalled!"
+                else
+                    echo
+                    echo "ERROR: Couldn't uninstall Murtop." >&2
+                fi
+                ;;
+#H -v, --version            Prints the script version.
+            -v|--version)
+                echo "$SCRIPT_VERSION"
+                ;;
+            *)
+                echo "ERROR: Invalid option '$1'." >&2
+                exit 2
+                ;;
+        esac
+    fi
+}
+
+
+function main() {
+    if ! is_sudo; then
+        echo "The script must be run under sudo." >&2
+        echo >&2
+        usage
+        exit 1
+    fi
+ 
+    if ! is_retropie; then
+        echo "RetroPie is not installed. Aborting ..." >&2
+        exit 1
+    fi
+
+    get_options "$@"
+}
+
+main "$@"
---
+++ scriptmodules/ports/murtop.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="murtop"
+rp_module_desc="Murtop - A fast-paced 80's arcade-style game where Dig Dug meets Bomberman."
+rp_module_license="MIT https://github.com/hiulit/RetroPie-Murtop/blob/main/LICENSE"
+rp_module_section="opt"
+rp_module_flags="x86 x86_64 arm aarch64"
+
+readonly GAME_BIN_DIR="/home/$user/RetroPie-Murtop/bin"
+
+
+function install_bin_murtop() {
+    local game_zip_file
+
+    game_zip_file="$(find "$GAME_BIN_DIR" -type f -name "*.zip")"
+    
+    if [[ "$?" -ne 0 ]]; then
+        fatalError "You must create '$GAME_BIN_DIR' and then place 'murtop_[VERSION]_[ARCH].zip' in it."
+    fi
+
+    if [[ -z "$game_zip_file" ]]; then
+        fatalError "ERROR: You must place 'murtop_[VERSION]_[ARCH].zip' in '$GAME_BIN_DIR'."
+    fi
+
+    unzip -o "$game_zip_file" -d "$md_inst"
+}
+
+
+function configure_murtop() {
+    local cmd
+
+    cmd="$(find "$md_inst" -not -name "retropie.pkg" -not -name "*.pck" -not -type d)"
+
+    if isPlatform "arm"; then
+        cmd+=" -f"
+    fi
+
+    addPort "$md_id" "murtop" "murtop" "$cmd"
+}
